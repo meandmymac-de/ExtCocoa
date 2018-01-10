3151f2b5b9141796d562f740029f637c48684166
Thomas Bonk <thomas@meandmymac.de>
2018-01-10 21:02:58 +0100

    Initial Commit

---
+++ ExtCocoa/ExtCocoa.h
@@ -0,0 +1,19 @@
+//
+//  ExtCocoa.h
+//  ExtCocoa
+//
+//  Created by Thomas Bonk on 10.01.18.
+//  Copyright © 2018 Thomas Bonk Softwareentwicklung. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ExtCocoa.
+FOUNDATION_EXPORT double ExtCocoaVersionNumber;
+
+//! Project version string for ExtCocoa.
+FOUNDATION_EXPORT const unsigned char ExtCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtCocoa/PublicHeader.h>
+
+
